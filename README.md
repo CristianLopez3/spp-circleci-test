3feeff295ef8d4b7189f6b6a374191ab33fcfac4
cristian <cristian.c.lopez.m@gmail.com>
2024-04-04 10:15:50 -0500

    initializr project

---
+++ README.md
@@ -0,0 +1,9 @@
+# Testing With Spring Boot
+
+At the moment to create a new spring boot app, spring boot automatically brings a bunch of 
+dependencies for testing, such as:
+* junit
+* assertj
+* mockito
+* hamcrest
+* json-path
---
+++ src/main/java/com/cristian/web/springbootteststarter/SpringBootTestStarterApplication.java
@@ -0,0 +1,13 @@
+package com.cristian.web.springbootteststarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTestStarterApplication.class, args);
+    }
+
+}
